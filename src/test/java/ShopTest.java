990a6be97db4570cb30d63925e6c5334223fcd28
Richard <fahitch+github@gmail.com>
2020-01-17 16:09:20 +0000

    add removeStock test to ShopTest

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,4 +1,5 @@
 import behaviours.ISell;
+import instruments.AcousticGuitar;
 import instruments.InstrumentType;
 import instruments.MoogSynth;
 import org.junit.Before;
@@ -6,17 +7,22 @@ import org.junit.Test;
 import shop.Shop;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class ShopTest {
 
     Shop shop;
     ISell moog;
+    ISell guitar;
 
     @Before
     public void before(){
         shop = new Shop("BeatHoven's");
         moog = new MoogSynth ("Jet Black", InstrumentType.KEYBOARD, 49, 250, 350);
+        guitar = new AcousticGuitar("oak brown", InstrumentType.STRING, 6, 100, 200);
     }
+
+}
     
     @Test
     public void hasName(){
@@ -33,5 +39,14 @@ public class ShopTest {
         shop.addStock(moog);
         assertEquals(1, shop.getStock().size());
         assertEquals(true, shop.getStock().contains(moog));
+    }
+
+    @Test
+    public void removeStock(){
+        shop.addStock(moog);
+        shop.addStock(guitar);
+        shop.removeStock(moog);
+        assertEquals(1, shop.getStock().size());
+        assertFalse(shop.getStock().contains(moog));
     }
 }
