976d5eebf275391650ee7ccc5f84766b505a1148
Richard <fahitch+github@gmail.com>
2020-01-17 11:50:58 +0000

    add interface IPlay

--- src/main/java/behaviours/IPlay.java
+++ src/main/java/behaviours/IPlay.java
@@ -2,4 +2,5 @@ package behaviours;
 
 public interface IPlay {
     public String play();
+    // This should have a play method that returns the sound of the instrument being played as a String.
 }
