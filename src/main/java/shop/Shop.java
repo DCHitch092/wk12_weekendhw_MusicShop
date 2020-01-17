a2b441f591c8465ad11fcbe6cfb63ab88b81b2fa
Richard <fahitch+github@gmail.com>
2020-01-17 16:03:21 +0000

    Shop passes hasName test

--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -1,4 +1,23 @@
 package shop;
 
+import behaviours.ISell;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Shop {
+
+    private String name;
+    private ArrayList<ISell> stock;
+
+    public Shop(String name) {
+        this.name = name;
+        this.stock = new ArrayList<ISell>();
+    }
+
+    public String getName() {
+        return name;
+    }
+//
+//    public ArrayList<ISell> getStock()
 }
